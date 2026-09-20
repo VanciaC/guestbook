e3ec128f5e322c53846b810347df4de4cc51ab15
ChanV <chan.vancia@gmail.com>
2026-09-20 23:13:48 +0200

    Add conference/comment interface with pagination

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,22 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
-    {
-        return new Response(<<<EOF
-                <html>
-                    <body>
-                        <img src="/images/under-construction.gif" />
-                    </body>
-                </html>
-                EOF
-        );
+    public function index(ConferenceRepository $conferenceRepository): Response    {
+        return $this->render('conference/index.html.twig', [
+             'conferences' => $conferenceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(
+        #[MapEntity] Conference $conference,
+        CommentRepository $commentRepository, 
+        #[MapQueryParameter(options: ['min_range' => 0])] int $offset = 0
+    ): Response {
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+        ]);
     }
 }
